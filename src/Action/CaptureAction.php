626fb721a0e62cc4e17eaa93f993a4f9e45e133e
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2015-03-16 09:15:29 +0000

    dry.

--- src/Action/CaptureAction.php
+++ src/Action/CaptureAction.php
@@ -5,29 +5,12 @@ use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\Exception\LogicException;
 use Payum\Core\Exception\RequestNotSupportedException;
 use Payum\Core\PaymentAwareInterface;
-use Payum\Core\PaymentInterface;
-use Payum\Core\Reply\HttpPostRedirect;
-use Payum\Core\Reply\HttpRedirect;
 use Payum\Core\Request\Capture;
-use Payum\Core\Request\GetHttpRequest;
 use Payum\Core\Request\ObtainCreditCard;
 use Payum\Core\Security\SensitiveValue;
 
-class CaptureAction extends BaseApiAwareAction implements PaymentAwareInterface
+class CaptureAction extends OffsiteCaptureAction implements PaymentAwareInterface
 {
-    /**
-     * @var PaymentInterface
-     */
-    protected $payment;
-
-    /**
-     * {@inheritDoc}
-     */
-    public function setPayment(PaymentInterface $payment)
-    {
-        $this->payment = $payment;
-    }
-
     /**
      * {@inheritDoc}
      *
@@ -35,27 +18,14 @@ class CaptureAction extends BaseApiAwareAction implements PaymentAwareInterface
      */
     public function execute($request)
     {
-        if (!$this->supports($request)) {
-            throw RequestNotSupportedException::createActionNotSupported($this, $request);
-        }
+        RequestNotSupportedException::assertSupports($this, $request);
 
         $details = ArrayObject::ensureArrayObject($request->getModel());
-
         if ($details['_status']) {
             return;
         }
 
-        if (false == $details['clientIp']) {
-            $this->payment->execute($httpRequest = new GetHttpRequest);
-
-            $details['clientIp'] = $httpRequest->clientIp;
-        }
-
-        if (isset($details['_completeCaptureRequired'])) {
-            $response = $this->gateway->completePurchase($details->toUnsafeArray())->send();
-
-            unset($details['_completeCaptureRequired']);
-        } else {
+        if (false == isset($details['_completeCaptureRequired'])) {
             if (false == $details->validateNotEmpty(array('card'), false) && false == $details->validateNotEmpty(array('cardReference'), false)) {
                 try {
                     $this->payment->execute($creditCardRequest = new ObtainCreditCard);
@@ -73,35 +43,8 @@ class CaptureAction extends BaseApiAwareAction implements PaymentAwareInterface
                     throw new LogicException('Credit card details has to be set explicitly or there has to be an action that supports ObtainCreditCard request.');
                 }
             }
-
-            $response = $this->gateway->purchase($details->toUnsafeArray())->send();
         }
 
-        if ($response->isRedirect()) {
-            $details['_completeCaptureRequired'] = 1;
-
-            if ($response->getRedirectMethod() == 'POST') {
-                throw new HttpPostRedirect($response->getRedirectUrl(), $response->getRedirectData());
-            }
-            else {
-                throw new HttpRedirect($response->getRedirectUrl());
-            }
-        }
-
-        $details['_reference']      = $response->getTransactionReference();
-        $details['_status']         = $response->isSuccessful() ? 'captured' : 'failed';
-        $details['_status_code']    = $response->getCode();
-        $details['_status_message'] = $response->isSuccessful() ? '' : $response->getMessage();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function supports($request)
-    {
-        return
-            $request instanceof Capture &&
-            $request->getModel() instanceof \ArrayAccess
-        ;
+        parent::execute($request);
     }
 }
